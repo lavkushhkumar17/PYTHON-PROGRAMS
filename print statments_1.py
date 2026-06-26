9efbb1c93b185f4c40b376ab80915f0387a7ed10
Lavkush Kumar <lavkushhkumar17@gmail.com>
2026-06-26 19:34:55 +0530

    Add files via upload

---
+++ print statments_1.py
@@ -0,0 +1,20 @@
+#one or more statements/objects in()
+""" object =value to print =required
+    sep=separator between objects=' '
+    end=string append at end='\n'
+    file=output destination=screen(sys.stdout)"""
+#object
+print("hello world")
+print(10)
+print("python",3.14)
+#separator
+"""print("A","B","C")
+print("A","B","C",sep="-")
+print("A","B","C",sep="*")"""
+#end
+"""print("hello",end="      ")
+print("world")"""
+#file
+"""f=open("output.txt","w")
+print("hello python",file=f)
+f.close()"""
