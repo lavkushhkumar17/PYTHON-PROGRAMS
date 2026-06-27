62f6f055727bb935fc67db8292d881e59461bee3
Lavkush Kumar <lavkushhkumar17@gmail.com>
2026-06-27 16:51:42 +0530

    Add files via upload

---
+++ Print & Comment_2.py
@@ -0,0 +1,8 @@
+#checking how to print inputs
+print("hello world")
+print(5)
+print('bye')
+print("hello",7)
+print(7*5)
+'''hello i am lavkush, integral university,'''
+""" hello i am lavkush, integral university,"""
