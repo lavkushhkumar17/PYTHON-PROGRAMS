c3632c842cc9c798df593b9daf1021a3483198f4
Lavkush Kumar <lavkushhkumar17@gmail.com>
2026-06-27 16:50:07 +0530

    Uncomment print statements and examples

--- print statments_1.py
+++ print statments_1.py
@@ -3,17 +3,25 @@
     sep=separator between objects=' '
     end=string append at end='\n'
     file=output destination=screen(sys.stdout)"""
+
+
 #object
 print("hello world")
 print(10)
-#print("python",3.14)
+print("python",3.14)
+
+
 #separator
 """print("A","B","C")
 print("A","B","C",sep="-")
 print("A","B","C",sep="*")"""
+
+
 #end
 """print("hello",end="      ")
 print("world")"""
+
+
 #file
 """f=open("output.txt","w")
 print("hello python",file=f)
