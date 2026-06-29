7c4fbbba453cd8065b2e00ebff602431fc0929dc
Lavkush Kumar <lavkushhkumar17@gmail.com>
2026-06-29 10:19:56 +0530

    Add files via upload

---
+++ Escape sequence ch_3.py
@@ -0,0 +1,20 @@
+""" *\n -new line
+    *\t -tab
+    *\\ -backslash
+    *\" -double quote
+    *\' -single quote
+    *\r -carriage return
+    *\b -backspace 
+    *\f -form feed
+    *\v -vertical tab
+    *\a -alert/bell    """
+print("hey i am a good boy\nand the viewer is also a good boy/girl")
+print("hello\tworld")
+print("c:\\users\\admin")
+print("he said,\"hi\"")
+print('it\'s python')
+print("hello\rhi")
+print("hello\b")
+print("hello\fworld")
+print("hello\vworld")
+print("\a")
