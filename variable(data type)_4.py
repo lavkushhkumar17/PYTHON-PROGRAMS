4132f64837a938311623b356849e8c3120c4d422
Lavkush Kumar <lavkushhkumar17@gmail.com>
2026-07-08 23:44:13 +0530

    Add files via upload

---
+++ variable(data type)_4.py
@@ -0,0 +1,70 @@
+#variable=container that holds data
+"""a=1=integers
+   b=true=boolean
+   c="harry"=string
+   d=none=type"""
+#data type
+"""a=1
+b=True
+c="harry"
+d=None
+print(a) #output1
+print(b)#output2
+a1=9
+print(a+a1)#output3
+print("the type of a is",type(a))#output4
+print("the type of b is",type(b))#output5
+print("the type of c is",type(c))#output6
+print("the type of d is",type(d))#output7"""
+
+
+
+#{NUMERIC DATA TYPE}=int=1,float=3.5,complex=3+6j
+"""a=1
+print(a)#output1
+b=1.1
+print(b)#output2
+c=8+2j
+print(c)#output3
+#or
+d=complex(8,2)
+print(d)#output3
+print("the type of class a is",type(a))
+print("the type of class b is",type(b))
+print("the type of class c is",type(c))
+print("the type of class d is",type(d))"""
+
+
+#{TEXT DATA}=string
+"""a="lavkush"
+print("the type of a is",type(a))"""
+
+
+
+#{BOOLEAN DATA}=true/false, yes/no
+"""a=True
+b=False
+print("value of a:",a)
+print("value of b:",b)
+print("the type of a is",type(a))
+print("the type of b is",type(b))"""
+
+
+
+#{SEQUENCED DATA}=list,tuple
+"""list1=[8,2.3,[-4,5],["apple","banana"]]
+print(list1)
+print("this is a type of",type(list1))
+tuple1=(("parrot","sparrow"),("lion","tiger"))
+print(tuple1)
+print("this is a type of",type(tuple1))"""
+
+
+
+#{MAPPED DATA}=dictionary=key:value pair, enclosed by curley bracket.
+"""dict1={"name":"lavkush","age":21,"canVote":True,"city":"lucknow"}
+print(dict1)"""
+
+
+
+# {EVERYTHING IN PYTHON IS AN OBJECT} LIKE STRING,INTEGER,BOOLEAN,LIST,TUPLE,DICTIONARY,SET,FLOAT,COMPLEX,FUNCTIONS,CLASSES,OBJECTS ETC.
